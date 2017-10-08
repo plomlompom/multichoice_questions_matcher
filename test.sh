9f1d3d9870abc2ef0eb9130f43c7c47d16b2c643
Christian Heller <c.heller@plomlompom.de>
2017-10-08 19:28:12 +0200

    Extend tests.

--- test.sh
+++ test.sh
@@ -5,7 +5,10 @@ set -x
 python3 -m unittest *.py
 ./matcher.py tests/foo.json tests/bar.json > /tmp/matcher.py_test_output
 diff /tmp/matcher.py_test_output tests/expected_matcher.py_test_output
-cat tests/answer.py_input | ./answer.py > /tmp/answer.py_test_output
+cat tests/answer.py_input_1 | ./answer.py > /tmp/answer.py_test_output
 diff /tmp/answer.py_test_output tests/expected_answer.py_test_output
+cp tests/bar.json tests/baz.json
+cat tests/answer.py_input_2 | ./answer.py --target tests/baz.json --source tests/foo.json
+diff tests/baz.json tests/expected_baz.json
 
 echo "TESTS SUCCEEDED."
